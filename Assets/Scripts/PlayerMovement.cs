71d2c45a1f115b9f90a7bdff01cbed0dd853a3f8
André Luiz Rodrigues de Oliveira <87727993+andreluiz14@users.noreply.github.com>
2023-02-07 17:06:57 -0300

    Movimentação e ajuste de camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,30 @@
 using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    public float jumpSpeed;
+    public float jumpButtonGracePeriod;
+
+    private Animator animator;
     private CharacterController characterController;
+    private float ySpeed;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressedTime;
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
 
     // Update is called once per frame
@@ -27,17 +40,49 @@ public class PlayerMovement : MonoBehaviour
 
         //Normalizar o vetor mantem a magnitute em 1
         movementDirection.Normalize();
+        
+        if (characterController.isGrounded)
+        {
+            lastGroundedTime = Time.time;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpButtonPressedTime = Time.time;
+        }
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+            if (Time.time - jumpButtonPressedTime <=  jumpButtonGracePeriod)
+            {
+                ySpeed = jumpSpeed;
+                jumpButtonPressedTime = null;
+                lastGroundedTime = null;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
 
         //transform.Translate(movementDirection * magnitude * Time.deltaTime * speed, Space.World);
-        characterController.SimpleMove(movementDirection * magnitude);
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime) ;
 
         //Caso o movimento for diferente de zero
         //Rotacionar o objeto de acordo com a direção do movimento
         if(movementDirection != Vector3.zero)
         {
+            animator.SetBool("IsMoving", true);
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
         }
     }
 }
